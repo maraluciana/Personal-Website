5761c688749c2a652f75a903ee26a9405996e1bd
maraluciana <maraa.lucianaa@yahoo.com>
2023-02-22 18:34:51 +0200

    defined routes

---
+++ personalwebsite.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+   return "Homepage"
+
+@app.route('/about')
+def about():
+   return "About"
+
+@app.route('/resume')
+def resume():
+   return "Resume"
+
+@app.route('/portofolio')
+def portofolio():
+   return "Portofolio"
+
+@app.route('/contact')
+def contact():
+   return "Contact"
+
+if __name__ == '__main__':
+    app.run(debug = True)
