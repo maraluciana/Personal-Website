3a8108aa82683fee91c7eb02b488c4c31a8e5e82
maraluciana <maraa.lucianaa@yahoo.com>
2023-03-13 20:26:21 +0200

    menu functional homepage and rest

--- personalwebsite.py
+++ personalwebsite.py
@@ -10,8 +10,6 @@ load_dotenv()
 
 app = Flask(__name__)
 
-
-
 def sendContactForm(result):
    try:
       email_sender = os.getenv('MAIL_USERNAME_SENDER')
@@ -45,21 +43,26 @@ def sendContactForm(result):
    else:
       return 'success'
 
+
+
+menu = 'menu'
+
 @app.route('/')
 def home():
-   return render_template("homepage.html")
+   menu = 'homepage_menu'
+   return render_template("homepage.html", menu = menu)
 
 @app.route('/about')
 def about():
-   return render_template("about.html")
+   return render_template("about.html", menu = menu)
 
 @app.route('/resume')
 def resume():
-   return render_template("resume.html")
+   return render_template("resume.html", menu = menu)
 
 @app.route('/portfolio')
 def portfolio():
-   return render_template("portfolio.html")
+   return render_template("portfolio.html", menu = menu)
 
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
@@ -76,9 +79,9 @@ def contact():
 
       alert_code = sendContactForm(result)
 
-      return render_template("contact.html", alert = alert_code)
+      return render_template("contact.html", alert = alert_code, menu = menu)
    
-   return render_template("contact.html", alert = alert_code)
+   return render_template("contact.html", alert = alert_code, menu = menu)
 
 
 
