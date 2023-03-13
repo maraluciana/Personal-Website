8b9b05307fcea8aa27adfc89cfc4415593ec6737
maraluciana <maraa.lucianaa@yahoo.com>
2023-03-13 21:26:56 +0200

    scroll effect on menu

---
+++ static/javascript/script.js
@@ -0,0 +1,10 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "0px";
+  } else {
+    document.getElementById("navbar").style.padding = "15px";
+  }
+}
