a919ab211350696bf36be5bf23cc0f82371b2fac
maraluciana <maraa.lucianaa@yahoo.com>
2023-03-14 20:04:16 +0200

    repaired title of navbar

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -3,15 +3,21 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   var navbar = document.getElementById("navbar");
   var navbarImage = document.getElementById("navbarimage");
+  var navbarTitle = document.getElementById("navbartitle");
+
   var collection = document.getElementsByClassName("white-text");
+  
 
   if (window.innerWidth > 960){
-    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
       navbar.style.backgroundColor = "white";
 
       for (let i = 0; i < collection.length; i++) {
-        collection[i].setAttribute('style', 'color:black !important');
+        collection[i].setAttribute('style', 'color:#444 !important');
       }
+
+      navbarTitle.setAttribute('style', 'color:#444 !important');
+      navbarTitle.style.fontSize = "20px";
 
       navbarImage.height = 60;
       navbarImage.width = 70;
@@ -21,8 +27,11 @@ function scrollFunction() {
       navbar.style.backgroundColor = "transparent";
 
       for (let i = 0; i < collection.length; i++) {
-        collection[i].setAttribute('style', 'color:white !important');
+        collection[i].setAttribute('style', 'color:#f7f7f7 !important');
       }
+
+      navbarTitle.setAttribute('style', 'color:#f7f7f7 !important');
+      navbarTitle.style.fontSize = "25px";
 
       navbarImage.height = 100;
       navbarImage.width = 120;
@@ -32,8 +41,11 @@ function scrollFunction() {
     navbar.style.backgroundColor = "white";
 
     for (let i = 0; i < collection.length; i++) {
-      collection[i].setAttribute('style', 'color:black !important');
+      collection[i].setAttribute('style', 'color:#444 !important');
     }
+
+    navbarTitle.setAttribute('style', 'color:#444 !important');
+    navbarTitle.style.fontSize = "20px";
 
     navbarImage.height = 100;
     navbarImage.width = 120;
