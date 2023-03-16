89197ac6b8ad98ddfe8e77a5841e73e3d835b1dc
maraluciana <maraa.lucianaa@yahoo.com>
2023-03-16 18:14:57 +0200

    navbar responsive to the screen's size change

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,5 +1,6 @@
 window.onscroll = function() {scrollFunction()};
 
+
 function scrollFunction() {
   var navbar = document.getElementById("navbar");
   var navbarImage = document.getElementById("navbarimage");
@@ -7,22 +8,83 @@ function scrollFunction() {
 
   var collection = document.getElementsByClassName("white-text");
 
-  for (let i = 0; i < collection.length; i++) {
-    collection[i].addEventListener("mouseover", function(){
-      this.style.color = "#1abc9c";
-    });
+  window.addEventListener('resize', function() { //the navbar is immediately responsive to the change of size of the screen
+    if (window.innerWidth < 960) { 
+      navbar.style.backgroundColor = "white";
 
-    collection[i].addEventListener("mouseout", function(){
-      this.setAttribute('style', 'color:#444 !important');
-    });
-  }
+      for (let i = 0; i < collection.length; i++) {
+        collection[i].setAttribute('style', 'color:#444 !important');
+  
+  
+        collection[i].addEventListener("mouseover", function(){
+          this.style.color = "#1abc9c";
+        });
+    
+        collection[i].addEventListener("mouseout", function(){
+          this.setAttribute('style', 'color:#444 !important');
+        });
+      }
+  
+      navbarTitle.setAttribute('style', 'color:#444 !important');
+      navbarTitle.style.fontSize = "20px";
+  
+      navbarImage.height = 100;
+      navbarImage.width = 120;
+    } 
+    else {
+      if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) { //big screen + scroll down
+        navbar.style.backgroundColor = "white";
+        
+        for (let i = 0; i < collection.length; i++) {
+          collection[i].setAttribute('style', 'color:#444 !important');
+  
+          collection[i].addEventListener("mouseover", function(){
+            this.style.color = "#1abc9c";
+          });
+      
+          collection[i].addEventListener("mouseout", function(){
+            this.setAttribute('style', 'color:#444 !important');
+          });
+        }
+  
+        navbarTitle.setAttribute('style', 'color:#444 !important');
+        navbarTitle.style.fontSize = "20px";
+  
+        navbarImage.height = 60;
+        navbarImage.width = 70;
+  
+      }
+      else{ // big screen + no scroll
+        navbar.style.backgroundColor = "transparent";
+  
+        for (let i = 0; i < collection.length; i++) {
+          collection[i].setAttribute('style', 'color:#f7f7f7 !important');
+  
+          collection[i].addEventListener("mouseover", function(){
+            this.style.color = "#1abc9c";
+          });
+      
+          collection[i].addEventListener("mouseout", function(){
+            this.setAttribute('style', 'color:#f7f7f7 !important');
+          });
+        }
+  
+        navbarTitle.setAttribute('style', 'color:#f7f7f7 !important');
+        navbarTitle.style.fontSize = "25px";
+  
+        navbarImage.height = 100;
+        navbarImage.width = 120;
+      }
+      
+    }
+  });
+  
 
   
   if (window.innerWidth > 960){
-    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) { //big screen + scroll down
       navbar.style.backgroundColor = "white";
       
-
       for (let i = 0; i < collection.length; i++) {
         collection[i].setAttribute('style', 'color:#444 !important');
 
@@ -42,7 +104,7 @@ function scrollFunction() {
       navbarImage.width = 70;
 
     }
-    else{
+    else{ // big screen + no scroll
       navbar.style.backgroundColor = "transparent";
 
       for (let i = 0; i < collection.length; i++) {
@@ -64,27 +126,30 @@ function scrollFunction() {
       navbarImage.width = 120;
     }
   }
-  else{
-    navbar.style.backgroundColor = "white";
-
-    for (let i = 0; i < collection.length; i++) {
-      collection[i].setAttribute('style', 'color:#444 !important');
+  else{ //small screen 
+        //already works no need for additional code
 
 
-      collection[i].addEventListener("mouseover", function(){
-        this.style.color = "#1abc9c";
-      });
+    // navbar.style.backgroundColor = "white";
+
+    // for (let i = 0; i < collection.length; i++) {
+    //   collection[i].setAttribute('style', 'color:#444 !important');
+
+
+    //   collection[i].addEventListener("mouseover", function(){
+    //     this.style.color = "#1abc9c";
+    //   });
   
-      collection[i].addEventListener("mouseout", function(){
-        this.setAttribute('style', 'color:#444 !important');
-      });
-    }
+    //   collection[i].addEventListener("mouseout", function(){
+    //     this.setAttribute('style', 'color:#444 !important');
+    //   });
+    // }
 
-    navbarTitle.setAttribute('style', 'color:#444 !important');
-    navbarTitle.style.fontSize = "20px";
+    // navbarTitle.setAttribute('style', 'color:#444 !important');
+    // navbarTitle.style.fontSize = "20px";
 
-    navbarImage.height = 100;
-    navbarImage.width = 120;
+    // navbarImage.height = 100;
+    // navbarImage.width = 120;
   }
   
 }
