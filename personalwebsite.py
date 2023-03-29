ef2725e30ca3426f478b8057f89316f827cf0128
maraluciana <maraa.lucianaa@yahoo.com>
2023-03-29 20:25:37 +0300

    added automated confirmation message to form

--- personalwebsite.py
+++ personalwebsite.py
@@ -44,6 +44,47 @@ def sendContactForm(result):
       return e
    else:
       return 'success'
+   
+
+def automatedMessage(result):
+   try:
+      email_sender = os.getenv('MAIL_USERNAME_SENDER')
+      email_receiver = result['email']
+      password = os.getenv('MAIL_PASSWORD')
+      mail_server = os.getenv('MAIL_SERVER')
+      mail_port = os.getenv('MAIL_PORT')
+
+      message = """
+         Thank you for your message. This is an automated response to let you know that I have received your message via marabelu.ro and will respond as soon as possible.
+         If your matter is urgent, please don't hesitate to call me at 0722618765. Otherwise, I will get back to you as soon as I can.
+
+         You can find below a summary of the information you provided in the contact form:
+         
+         Name : {}
+         Email : {}
+      
+         Message : {}
+         
+         Best regards,
+         Mara-Luciana Belu
+         """.format(result['name'],result['email'],result['message'])
+
+      em = EmailMessage()
+      em['From'] = email_sender
+      em['To'] = email_receiver
+      em['Subject'] = result['subject']
+      em.set_content(message)
+      
+      context = ssl.create_default_context()
+
+      with smtplib.SMTP_SSL(mail_server, mail_port, context=context) as smtp:
+         smtp.login(email_sender, password)
+         smtp.sendmail(email_sender, email_receiver, em.as_string())
+   except Exception as e:
+      return e
+   else:
+      return 'success'
+
 
 
 @app.errorhandler(404)
@@ -69,6 +110,7 @@ def portfolio():
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
    alert_code = 'waiting'
+
    logging = os.getenv('LOGGING')
 
    if request.method == 'POST':
@@ -81,6 +123,7 @@ def contact():
 
 
       alert_code = sendContactForm(result)
+      automatedMessage(result)
 
       return render_template("contact.html", alert = alert_code, logging = logging)
    
